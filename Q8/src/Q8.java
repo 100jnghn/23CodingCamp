215f38e637d83974a52abb2a4d18ac277583d129
100jnghn <gnswhd0415@gmail.com>
2023-01-09 22:00:19 +0900

    2023 코딩캠프 1일차 / 미완성

--- Q8/src/Q8.java
+++ Q8/src/Q8.java
@@ -1,10 +1,11 @@
+//미완성
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
-//
+
 public class Q8 {
 
 	public static void main(String[] args) throws IOException {
